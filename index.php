5b604293199d88658d2a2de91ef494692f720312
Szymon Janecki <szjanecki@gmail.com>
2020-11-21 10:25:07 +0100

    add header, menu, footer and form without styles

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php echo "string";
+<?php
+
+include 'includes/header.php';
+
+?>
+<div class='content'>
+  <div class='form'>
+    <form>
+      <label for="forname"> First name: </label>
+      <input type="text" id="forname" name="forname"><br>
+      <label for="surname"> Last name: </label>
+      <input type="text" id="surname" name="surname"><br>
+      <label for="email"> E-mail: </label>
+      <input type="text" id="email" name="email"><br>
+      <label for="message"> Message: </label>
+      <input type="text" id="message" name="message"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+</div>
+
+<?php
+include 'includes/footer.php';
 
 ?>
