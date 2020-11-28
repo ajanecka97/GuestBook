b07d95af1e4321ecd0a2c500ef9d4483bd5358d1
Szymon Janecki <szjanecki@gmail.com>
2020-11-28 15:02:24 +0100

    Add contact/info pages. Visual enhancement

---
+++ GuestBook/contact.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="css/menu.css">
+   <link rel="stylesheet" href="css/styles.css">
+   <title>Guest Book</title>
+</head>
+<body>
+
+<?php
+
+include 'includes/menu.php';
+
+?>
+<div class='content'>
+  <div class='border3'>
+      <p>Contact us!</p>
+
+      <table>
+        <tr>
+          <td> Email: </td>
+          <td> example@example.com </td>
+        </tr>
+        <tr>
+          <td> Tel: </td>
+          <td> 666666666 </td>
+        </tr>
+  </div>
+</div>
+</body>
+</html>
---
+++ GuestBook/info.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="css/menu.css">
+   <link rel="stylesheet" href="css/styles.css">
+   <title>Guest Book</title>
+</head>
+<body>
+
+<?php
+
+include 'includes/menu.php';
+
+?>
+<div class='content'>
+  <div class='border3'>
+    <p>Qui omnis ipsa consectetur adipisci vel numquam omnis ipsa. Et consectetur eius voluptatum. Quaerat sequi ipsa rem natus sed rerum. Quasi fugiat enim vel repudiandae et quis ut.<p>
+
+<p>Veniam ipsa qui quo. Cum totam provident cumque. Expedita aut rerum dolorem enim sed voluptates consequatur. </p>
+
+<p>Et magnam voluptates dolores. Alias architecto quasi qui nemo. Nihil veritatis illo odit rerum explicabo optio. Ut distinctio qui natus fugiat aperiam fugiat consequatur. Aperiam sed libero excepturi sunt est. Ducimus qui rerum unde sint dolorum sequi.</p>
+
+<p>Laudantium reprehenderit temporibus fugiat mollitia. Cumque voluptatibus deserunt perspiciatis quasi et. Perspiciatis illum deserunt et. Neque harum nihil amet ea perferendis corporis.</p>
+
+<p>Amet necessitatibus et perferendis magni sit. Expedita excepturi aut laborum enim maiores. Possimus nesciunt nulla quia aut et sit excepturi dignissimos. Iste aliquid ut eveniet dolor nemo modi ut.</p>
+  </div>
+</div>
+</body>
+</html>
